ec039de86155831b61c1a2e7c38e5b80beeb1160
nsohit <52670389+nsohit@users.noreply.github.com>
2019-12-24 15:08:24 +0700

    Add files via upload

---
+++ adafruit_gps_acelero.ino
@@ -0,0 +1,115 @@
+// Adafruit IO Analog In Example
+// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-analog-input
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+// set up the 'analog' feed
+AdafruitIO_Feed *location = io.feed("gps");
+AdafruitIO_Feed *sent2 = io.feed("mpu");
+
+static const int RXPin = D3, TXPin = D4;
+static const uint32_t GPSBaud = 115200;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+void setup() {
+
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  ss.begin(GPSBaud);
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  // grab the current state of the photocell
+   Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  Serial.print("AcX = "); Serial.print(AcX);
+  delay(333);
+  
+double lati = (gps.location.lat(), 6);
+double longi = (gps.location.lng(), 6);
+    while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(lati);
+      Serial.print(" Longitude= "); 
+      Serial.println(longi);
+    }
+  }
+
+  // save the current state to the analog feed
+  Serial.print("sending -> ");
+  Serial.print(lati);
+  Serial.print(longi);
+  Serial.print(AcX);
+  location->save(lati ,longi);
+  sent2->save(AcX);
+  
+
+
+  // wait three seconds (1000 milliseconds == 1 second)
+  //
+  // because there are no active subscriptions, we can use delay()
+  // instead of tracking millis()
+  delay(3000);
+}
